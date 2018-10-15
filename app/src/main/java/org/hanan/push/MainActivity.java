82fc7db36b79f5053eadb4ee50c970c295da2030
Hanan Gitliz <hanan@angelsense.com>
2018-10-15 08:30:30 +0300

    move send to server inside messaging service.

--- app/src/main/java/org/hanan/push/MainActivity.java
+++ app/src/main/java/org/hanan/push/MainActivity.java
@@ -21,28 +21,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
-    private final static String IP = "192.168.2.108";
 
     private TextView text;
     private String classToken;
@@ -83,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
 
                                 // Get new Instance ID token
                                 String token = task.getResult().getToken();
+                                new TokenSender(token).execute();
 
                                 // Log and toast
                                 Log.i(TAG, "token!!!: " + token);
                                 text.setText(token);
-                                sendToServer(token);
 
                                 try {
                                     File f = new File("/sdcard/Angelsense/token.txt");
@@ -101,11 +87,6 @@ public class MainActivity extends AppCompatActivity {
                                     e.printStackTrace();
                                 }
                                 Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
-                            }
-
-                            private void sendToServer(String token) {
-                                classToken = token;
-                                new TokenSender().execute();
                             }
                         });
             }
@@ -136,13 +117,15 @@ public class MainActivity extends AppCompatActivity {
 
     private class TokenSender extends AsyncTask<String, Void, String> {
 
+        private final String token;
+
+        public TokenSender(String token) {
+            this.token = token;
+        }
+
         @Override
         protected String doInBackground(String... params) {
-            HashMap<String, String> map = new HashMap<>();
-            map.put("isGcm" ,"false");
-            map.put("token" ,classToken);
-            String response = performPostCall("http://" + IP + ":8080/api/v1/token", map);
-            Log.i(TAG, "send to server " + response);
+            MyFirebaseMessagingService.sendRegistrationToServer(token);
             return "Executed";
         }
 
@@ -157,64 +140,5 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(Void... values) {}
-
-        private String  performPostCall(String requestURL, HashMap<String, String> postDataParams) {
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(requestURL);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getPostDataString(postDataParams));
-
-                writer.flush();
-                writer.close();
-                os.close();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-
-        private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
-            StringBuilder result = new StringBuilder();
-            boolean first = true;
-            for(Map.Entry<String, String> entry : params.entrySet()){
-                if (first)
-                    first = false;
-                else
-                    result.append("&");
-
-                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-                result.append("=");
-                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-            }
-
-            return result.toString();
-        }
     }
 }
--- app/src/main/java/org/hanan/push/MyFirebaseMessagingService.java
+++ app/src/main/java/org/hanan/push/MyFirebaseMessagingService.java
@@ -2,14 +2,29 @@ package org.hanan.push;
 
 
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    //private final static String IP = "192.168.2.108";
+    private final static String IP = "10.0.0.4";
 
     /**
      * Called when message is received.
@@ -98,9 +113,69 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param token The new token.
      */
-    private void sendRegistrationToServer(String token) {
-        // TODO: Implement this method to send token to your app server.
+    public static void sendRegistrationToServer(String token) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("isGcm" ,"false");
+        map.put("token" ,token);
+        String response = performPostCall("http://" + IP + ":8080/api/v1/token", map);
     }
 
+    private static String performPostCall(String requestURL, HashMap<String, String> postDataParams) {
 
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private static String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
 }
